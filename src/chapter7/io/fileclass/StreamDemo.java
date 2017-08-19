ab6a32747704e56eb543d8d0e20ca781fb6bbd8a
Areg <areghakobyan9@gmail.com>
2017-08-19 23:38:30 +0400

    chapter7

---
+++ src/chapter7/io/fileclass/StreamDemo.java
@@ -0,0 +1,63 @@
+package chapter7.io.fileclass;
+
+import java.io.*;
+
+/**
+ * Created by karenvardanyan on 8/5/17.
+ */
+public class StreamDemo {
+
+	public static void main(String[] args) {
+		File file = new File("file.txt");
+		byte[] buff = readFile(file);
+		String fileContent = new String (buff);
+		System.out.println(fileContent);
+
+		writeFile(fileContent, new File("file_copy.txt"));
+
+
+	}
+
+	static byte[] readFile (File file) {
+		InputStream in = null;
+		byte [] buff = null;
+		try {
+			in = new FileInputStream(file);
+			buff = new byte[in.available()];
+			in.read(buff);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			close(in);
+		}
+		return buff;
+	}
+
+	static void writeFile(String content, File file) {
+		OutputStream out = null;
+
+		try {
+			out = new FileOutputStream(file);
+
+			out.write(content.getBytes());
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			close(out);
+		}
+	}
+
+	static void close (Closeable closeable){
+		if (closeable != null) {
+			try {
+				closeable.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+
+}
